0aeb48625cfa443faecd6a99c2c7ff6637070ff8
Stone <hityangxu@gmail.com>
2015-07-18 23:33:51 +0800

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nodepadApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the nodepadApp
+ */
+angular.module('nodepadApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nodepadApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nodepadApp
+ */
+angular.module('nodepadApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
